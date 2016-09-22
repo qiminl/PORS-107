2bc043d6c6a59ebc982c99e3804203500cb903e8
edward <gsngsngsn@gmail.com>
2016-09-21 21:36:51 -0700

    hmmm

--- FixVtVe.js
+++ FixVtVe.js
@@ -1,14 +1,15 @@
 data = '[{'+
             '"ad_slot_id"   :117,   '+
-            '"current_view_count"  :0,    '+
+            '"current_view_count"  :0,'+
             '"current_vt"   :0,     '+
             '"target_vt"    :2000,   '+
             '"current_ve"   :0,     '+
             '"target_ve"    :65     '+
+            '"target_view_count" : 0'+
         '}]';
 
 //global variable constructed the request url.
-var vt, ve, id,target_imp= 0;
+var  id,target_imp, target_view_count, update_view_count= 0;
 var image_url, click_url, target_view_url = "";
 
 /**
@@ -16,43 +17,46 @@ var image_url, click_url, target_view_url = "";
  */
 function getVtVe(){
     var x = document.getElementById("frm1");
-    target_imp = parseInt(x.current_view_count.value) + 1;
-    if (x.target_ve.value >=100 || x.target_ve.value <0){
+    target_view_count = x.target_view_count.value;
+    update_view_count = target_view_count - x.current_view_count.value;
+    if (target_view_count == 0){
+        target_view_count = parseInt(x.current_view_count.value) + 1;
+        update_view_count = target_view_count - x.current_view_count.value;
+    }
+    else if(x.target_ve.value >=100 || x.target_ve.value <0){
         alert ("ve should be in range of [0,100]");
-    }else if (target_imp<=x.current_view_count.value){
-        alert ("target_imp less than current_view_count, wrong data.")
-    } else {
+    }else {
         id = x.ad_slot_id.value;
-        vt = x.target_vt.value * target_imp
-            - x.current_vt.value * x.current_view_count.value;
-        ve = x.target_ve.value * target_imp
-            - x.current_ve.value * x.current_view_count.value;
-        requestVeVt(id, vt, ve, target_imp);
+        var vt = x.target_vt.value * target_view_count
+            - x.current_vt.value *x.current_view_count.value;
+        var ve = x.target_ve.value * target_view_count
+            - x.current_ve.value *x.current_view_count.value;
+        requestVeVt(id, vt, ve, target_view_count, update_view_count);
     }
 }
 
 /**
  * Compile from local data.
- */
+
 function getDataValue() {
 
     var output = JSON.parse(data);
-    target_imp = output[0].target_imp;
-    if (target_imp<=0 )
-        target_imp = parseInt(output[0].current_view_count) + 1;
+    target_imp = output[0].target_view_count;
+    if (target_view_count<=0 )
+        target_view_count = parseInt(output[0].current_view_count) + 1;
 
-    if (target_imp<=output[0].current_view_count){
-        alert ("target_imp less than current_view_count, wrong data.")
+    if (target_view_count<=output[0].current_view_count){
+        alert ("target_view_count less than current_view_count, wrong data.")
     } else {
         id = output[0].ad_slot_id;
-        vt = output[0].target_vt * target_imp 
+        vt = output[0].target_vt * target_view_count 
             - output[0].current_vt * output[0].current_view_count;
-        ve = output[0].target_ve * target_imp 
+        ve = output[0].target_ve * target_view_count 
             - output[0].current_ve * output[0].current_view_count;
-        requestVeVt(id , vt, ve, target_imp);
+        requestVeVt(id , vt, ve, target_view_count);
     }
 }
-
+ */
 
 
 /**
@@ -62,23 +66,24 @@ function getDataValue() {
  * @param {int}   vt    calculated vt value
  * @param {int}   ve    calculated ve value
  */
-function requestVeVt(id, vt, ve, target_imp){    
+function requestVeVt(id, vt, ve,target_view_count, update_view_count){    
     console.log("id:",id ," vt:",vt, " ve:", ve);
     var error_message = "";
 
     if (id <=0 ){
         error_message = "id incorrect, Must have ad slot id.";
         alert (error_message);
-    } else if (target_imp <=0){
-        error_message= "Please corret your current impression value.\n";
-        alert (error_message);
-    } else if (vt<=100 || ve/target_imp>100 || ve/target_imp<=0){
+    } else if (update_view_count <=0){
+        alert ("view count not correctly setted");
+    } else if (vt<=100 || ve/target_view_count>100 || ve/target_view_count<=0){
         error_message = "vt value should not be less than 100ms.\n"
         +"ve value should be be >= 100 or <= 0";
-        alert (error_message);
+        alert ("haha", vt, "hmm:", ve);
+        console.log("haha", vt, "hmm:", ve);
+
     } else {
         var url = "http://adserver.vradx.com/sdk?p=" + id;
-        getData(url,checkResponse);
+        getData(url,checkResponse, vt, ve, update_view_count);
         
     }
 }
@@ -102,12 +107,12 @@ function createXHR(){
  * @param {string}   url        request url
  * @param {[Object]} callback   call back function
  */
-function getData(url, callback){
+function getData(url, callback, vt, ve, update_view_count){
     var xhr =  createXHR(); //新建一个XMLHttpRequest对象
     xhr.open("GET",url);      //打开一个请求
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4 && callback){
-            callback(xhr);    //call back fucntion.
+            callback(xhr, vt, ve, update_view_count);    //call back fucntion.
         }
     }
     xhr.send(null);           //send request
@@ -118,15 +123,17 @@ function getData(url, callback){
  *
  * @param {XMLHttpRequest}  res     response  
  */
-function  checkResponse(res){
+function  checkResponse(res, vt, ve, update_view_count){
 
     if (res.status === 200){
         var inside = JSON.parse(res.response);
         var view_stat_url = inside[id].view_stat_url;
         if( view_stat_url != null){
-            target_view_url = view_stat_url.replace("${VIEW_TIME}", vt);
-            target_view_url = target_view_url.replace("${VIEW_EFFECTIVENESS}", ve);
-            updateVtVe (target_view_url);
+            target_view_url = view_stat_url.replace("${VIEW_TIME}", vt/update_view_count);
+            target_view_url = target_view_url.replace("${VIEW_EFFECTIVENESS}", ve/update_view_count);
+            for (var i =0; i< update_view_count; i++){
+                updateVtVe (target_view_url, vt, ve);
+            }
         }else{
             target_view_url= null;
             alert ("Check your ID maybe? Null view_stat_url in response : ", view_stat_url);
@@ -154,7 +161,7 @@ function  checkResponse(res){
  *
  * @param {string} url   request url
  */
-function updateVtVe (url){
+function updateVtVe (url, vt, ve){
     var xhr2 =  createXHR(); 
     xhr2.open("GET",url);                 
     xhr2.send(null);     
@@ -162,8 +169,8 @@ function updateVtVe (url){
     if (xhr2.readyState === 1){
         document.getElementById("demo").innerHTML = target_view_url;
         var message = "Sent vt = " + vt + "; ve = " + ve +
-            "\nExpected value: vt= "+ vt/target_imp + "; ve = " + 
-            ve/target_imp + ";  imp = " + target_imp;
+            "\nExpected value: vt= "+ vt/target_view_count + "; ve = " + 
+            ve/target_view_count + ";  imp = " + target_view_count;
         alert(message);
     }
 }
